21f5f81ab4436d7cb0c7cb8bc3c96f217e201a56
dmshutof <dmshutof@yandex.ru>
2022-01-18 14:22:51 +0300

    Initial dist subtree commit

---
+++ js/chunk-37406a73.09ba3f3d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-37406a73"],{"6c9d":function(t,n,c){},"953c":function(t,n,c){"use strict";c("6c9d")},9601:function(t,n,c){"use strict";c.r(n);var e=function(){var t=this,n=t.$createElement,c=t._self._c||n;return c("div",{staticClass:"hit-the-floor"},[t._v("404")])},s=[],a={name:"Page404"},i=a,u=(c("953c"),c("2877")),o=Object(u["a"])(i,e,s,!1,null,"f423bbe0",null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-37406a73.09ba3f3d.js.map
